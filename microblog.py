a2ec3e09a259ebf21da8468a55f383cd12a28cd5
serhiion <onyserhii@gmail.com>
2021-09-20 13:55:24 +0300

    Revert "finally"
    This reverts commit 350bb030cd447da8353e2695592491d595c598a8.

--- microblog.py
+++ microblog.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Post, Notification, Message, Task
+from app.models import User, Post
 
 app = create_app()
 cli.register(app)
@@ -8,9 +8,3 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification, 'Task': Task}
